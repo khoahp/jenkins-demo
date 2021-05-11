b0ad09a60093c62a8dff6f502622160015ceb778
khoa <khoavd.it@gmail.com>
2021-05-11 16:38:00 +0700

    Update createJob

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,24 @@
+/*
 pipelineJob('pipelineJob') {
     definition {
         cps {
             script(readFileFromWorkspace('pipelineJob.groovy'))
             sandbox()
+        }
+    }
+}*/
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Build'
+            }
+        }
+        stage('Test'){
+            steps {
+                echo 'Test'
+            }
         }
     }
 }
