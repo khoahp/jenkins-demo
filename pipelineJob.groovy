098f18722299293e583b42d68d0c4987cd501521
khoavu <khoavu@khoavus-MacBook-Pro.local>
2021-05-18 17:16:58 +0700

    Update

--- pipelineJob.groovy
+++ pipelineJob.groovy
@@ -27,9 +27,10 @@ def getInputParams(def param1, def param2) {
     if(x)
         println "x value: ${x}"
 
-    String config = libraryResource 'config/smocktest.json'
+    //String config = libraryResource 'smocktest.json'
 
-    def props = readJSON text: config
+    //def props = readJSON text: config
+    def props = readJSON text: '{"tools-devportals":" 01-12-2021","mobile-authz":" 01-11-2021"}'
 
     props.each { key, value ->
         echo "Walked through key $key and value $value"
