b8cfd01dca647961d0414ea11f0d1729997d830f
khoa <khoavd.it@gmail.com>
2021-05-19 15:08:37 +0700

    Update

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,5 +1,7 @@
 import groovy.json.JsonSlurper
 
+testFunc()
+
 pipelineJob('pipelineJob') {
     definition {
         cps {
@@ -8,7 +10,7 @@ pipelineJob('pipelineJob') {
         }
     }
 
-    testFunc()
+
 }
 
 pipelineJob('backend-pipe') {
